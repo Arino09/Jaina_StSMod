775fb944d4cc47567c8e6b5ca8fe5bd380ea347a
Arino <dong.yuanhao@outlook.com>
2023-01-28 01:41:00 +0800

    修正：法术反制显示错误为错误使用了fadingOut属性，虚无属性应为isEthereal。 TempCard类改为OptionCard类并添加了法术反制选项卡的实现类；OptionCard类对选项卡画的选择方式有两个构造方法，并相应地在AbstractJainaCard中添加了构造方法。删除了法术反制对应的特殊代码。 完全实现了冰冻效果，完成了FrozenMonsterPatch : 实现了修改怪物意图UI和描述的方法，本地化文本在ui.json中，能力icon和意图icon更新。 卡牌更新：衍生物全部改为SPECIAL稀有度，冰霜新星和冰锥术改为UNCOMMON稀有度，添加法术反制对应的衍生物。 能力更新：蒸发不会在回合结束时减少，只会在收到攻击时减少。问题：对于多段伤害，蒸发应该只能造成1次反伤。

--- src/main/java/Jaina/cards/Strike.java
+++ src/main/java/Jaina/cards/Strike.java
@@ -4,6 +4,7 @@ import Jaina.ModCore.IHelper;
 import Jaina.ModCore.JainaEnums;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
