783ebe789826ce65fbae43e402aeb56250895f58
Arino <dong.yuanhao@outlook.com>
2023-01-28 01:33:40 +0800

    OptionCard构造方法只能使用本卡画或类型测试图画

--- src/main/java/Jaina/cards/optionCards/OptionCard.java
+++ src/main/java/Jaina/cards/optionCards/OptionCard.java
@@ -1,8 +1,6 @@
 package Jaina.cards.optionCards;
 
 import Jaina.cards.AbstractJainaCard;
-import Jaina.cards.Counterspell;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.localization.CardStrings;
 
 public abstract class OptionCard extends AbstractJainaCard {
@@ -11,34 +9,29 @@ public abstract class OptionCard extends AbstractJainaCard {
     private static final CardColor COLOR = CardColor.COLORLESS;
     private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.NONE;
-    /**
-     * 使用其他卡画的选项卡牌
-     * @param id 完整卡牌ID
-     * @param card 使用该牌的卡画
-     * @param cardStrings 本地化字段
-     * @param type 卡牌类型
-     */
-    public OptionCard(String id, AbstractJainaCard card, CardStrings cardStrings, CardType type) {
-        super(id, cardStrings, getImgPath(card.type, card.cardID), COST, type, COLOR, RARITY, TARGET);
-    }
+
     /**
      * 使用本卡画的选项卡牌
-     * @param id 完整卡牌ID
+     *
+     * @param id          完整卡牌ID
+     * @param useTestArt  是否使用测试图片
      * @param cardStrings 本地化字段
-     * @param type 卡牌类型
+     * @param type        卡牌类型
      */
-    public OptionCard(String id, CardStrings cardStrings, CardType type) {
-        super(id, cardStrings, getOptImgPath(id), COST, type, COLOR, RARITY, TARGET);
+    public OptionCard(String id, boolean useTestArt, CardStrings cardStrings, CardType type) {
+        super(id, cardStrings, useTestArt ? getTestImgPath(type) : getOptImgPath(id), COST, type, COLOR, RARITY, TARGET);
     }
 
     /**
      * 获取选项卡牌路径
+     *
      * @param id 完整卡牌ID
      * @return 卡画路径
      */
     public static String getOptImgPath(String id) {
         return "Jaina/img/cards/option/" + id.substring(6) + ".png";
     }
+
     @Override
     public void upgrade() {
         if (!upgraded) {
