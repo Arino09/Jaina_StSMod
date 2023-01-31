8f6a4794768c1f2a9968b903a95dceae66bd1361
Arino <dong.yuanhao@outlook.com>
2023-01-31 17:56:57 +0800

    添加了烧伤关键词和能力。

---
+++ src/main/java/jaina/cards/Combustion.java
@@ -0,0 +1,51 @@
+package jaina.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+import jaina.powers.BurningPower;
+
+
+public class Combustion extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("Combustion");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = 1;
+
+    public Combustion() {
+        super(ID, false, CARD_STRINGS, COST, CardType.ATTACK, JainaEnums.JAINA_COLOR,
+                CardRarity.UNCOMMON, CardTarget.ENEMY, JainaEnums.CardTags.FIRE);
+        setDamage(8);
+        setMagicNumber(2);
+        setDamageType(JainaEnums.DamageType.FIRE);
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster monster) {
+        dealDamage(monster, AbstractGameAction.AttackEffect.FIRE);
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            if(!m.equals(monster)) {
+                givePower(new BurningPower(m, magicNumber), magicNumber);
+            }
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Combustion();
+    }
+
+}
