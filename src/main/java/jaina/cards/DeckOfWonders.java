81715e6408d0c1ccc547fea0e391138383db8671
Arino <dong.yuanhao@outlook.com>
2023-02-03 22:04:41 +0800

    添加了惊奇套牌和衍生物

---
+++ src/main/java/jaina/cards/DeckOfWonders.java
@@ -0,0 +1,45 @@
+package jaina.cards;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+
+
+public class DeckOfWonders extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("DeckOfWonders");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = 3;
+
+    public DeckOfWonders() {
+        super(ID, false, CARD_STRINGS, COST, CardType.SKILL, JainaEnums.JAINA_COLOR,
+                CardRarity.RARE, CardTarget.NONE, JainaEnums.CardTags.ARCANE);
+        this.exhaust = true;
+        setMagicNumber(3);
+        this.cardsToPreview = new ScrollOfWonder();
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(2);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < magicNumber; i++) {
+            addToBot(new MakeTempCardInDrawPileAction(new ScrollOfWonder(), 1, true, true));
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new DeckOfWonders();
+    }
+
+}
---
+++ src/main/java/jaina/cards/ScrollOfWonder.java
@@ -0,0 +1,54 @@
+package jaina.cards;
+
+import com.evacipated.cardcrawl.mod.stslib.actions.common.AutoplayCardAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+
+public class ScrollOfWonder extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("ScrollOfWonder");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = -2;
+
+    public ScrollOfWonder() {
+        super(ID, false, CARD_STRINGS, COST, CardType.SKILL, JainaEnums.JAINA_COLOR,
+                CardRarity.SPECIAL, CardTarget.NONE, JainaEnums.CardTags.ARCANE);
+        this.purgeOnUse = true;
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        addToBot(new AutoplayCardAction(this, AbstractDungeon.player.hand));
+    }
+
+    @Override
+    public void upp() {
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        // 随机生成一张吉安娜卡牌
+        AbstractCard card = IHelper.getRandomJainaCards(1).get(0);
+        // 免费释放卡牌且不留痕迹
+        card.purgeOnUse = true;
+        card.ignoreEnergyOnUse = true;
+        addToBot(new VFXAction(new ShowCardBrieflyEffect(card, 300, 300)));
+        card.use(p, AbstractDungeon.getRandomMonster());
+        drawCards(1);
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new ScrollOfWonder();
+    }
+
+}
