8b6e939868e629397ad4df208db9d15c81b4589c
Arino <dong.yuanhao@outlook.com>
2023-02-04 02:22:54 +0800

    添加了炽热连击

---
+++ src/main/java/jaina/cards/HotStreak.java
@@ -0,0 +1,43 @@
+package jaina.cards;
+
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.powers.FrostAffinityPower;
+import jaina.powers.HotStreakPower;
+
+
+public class HotStreak extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("HotStreak");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = 0;
+
+    public HotStreak() {
+        super(ID, false, CARD_STRINGS, COST, CardType.SKILL, JainaEnums.JAINA_COLOR,
+                CardRarity.COMMON, CardTarget.NONE, JainaEnums.CardTags.FIRE);
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upp() {
+        this.exhaust = false;
+        upgradeDescription(CARD_STRINGS);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        gainPower(new HotStreakPower(p));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new HotStreak();
+    }
+
+}
---
+++ src/main/java/jaina/powers/HotStreakPower.java
@@ -0,0 +1,50 @@
+package jaina.powers;
+
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import jaina.actions.RestoreCostAction;
+import jaina.actions.ReduceCostAction;
+import jaina.modCore.IHelper;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import jaina.modCore.JainaEnums;
+
+public class HotStreakPower extends AbstractJainaPower {
+    public static final String POWER_ID = IHelper.makeID("FrostAffinityPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    private static final String NAME = powerStrings.NAME;
+    private static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public HotStreakPower(AbstractCreature owner) {
+        super(POWER_ID, true, NAME, PowerType.BUFF);
+        this.owner = owner;
+        this.amount = -1;
+        updateDescription();
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        addToBot(new RestoreCostAction(JainaEnums.CardTags.FIRE));
+        addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+
+    @Override
+    public void onInitialApplication() {
+        flash();
+        addToBot(new ReduceCostAction(JainaEnums.CardTags.FIRE));
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        addToBot(new RestoreCostAction(JainaEnums.CardTags.FIRE));
+        addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+
+}
