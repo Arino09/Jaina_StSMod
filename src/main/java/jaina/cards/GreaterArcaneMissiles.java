5006a5113d1e5b8295a90385dbfa050c8a0cf273
Arino <dong.yuanhao@outlook.com>
2023-02-05 04:47:14 +0800

    添加了强能奥术飞弹

---
+++ src/main/java/jaina/cards/GreaterArcaneMissiles.java
@@ -0,0 +1,45 @@
+package jaina.cards;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+
+
+public class GreaterArcaneMissiles extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("GreaterArcaneMissiles");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = 2;
+    private final ArcaneMissileToken token = new ArcaneMissileToken();
+
+    public GreaterArcaneMissiles() {
+        super(ID, false, CARD_STRINGS, COST, CardType.SKILL, JainaEnums.JAINA_COLOR,
+                CardRarity.UNCOMMON, CardTarget.NONE, JainaEnums.CardTags.ARCANE);
+        setMagicNumber(3);
+        token.upgrade();
+        cardsToPreview = token;
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < magicNumber; i++) {
+            IHelper.getTempCard(token);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new GreaterArcaneMissiles();
+    }
+
+}
