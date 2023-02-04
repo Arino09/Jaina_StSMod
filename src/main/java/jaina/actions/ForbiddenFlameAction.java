bcca8b29e804d21f50a3841d4beeba0b1c770fe6
Arino <dong.yuanhao@outlook.com>
2023-02-05 04:46:58 +0800

    添加了禁忌烈焰

---
+++ src/main/java/jaina/actions/ForbiddenFlameAction.java
@@ -0,0 +1,50 @@
+package jaina.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import jaina.modCore.IHelper;
+
+public class ForbiddenFlameAction extends AbstractGameAction {
+    private final AbstractPlayer p;
+    private final boolean freeToPlayOnce;
+    private final int energyOnUse;
+
+    public ForbiddenFlameAction(AbstractPlayer p, AbstractMonster m, int amount, boolean freeToPlayOnce, int energyOnUse) {
+        this.amount = amount;
+        this.p = p;
+        this.target = m;
+        this.freeToPlayOnce = freeToPlayOnce;
+        this.duration = Settings.ACTION_DUR_XFAST;
+        this.actionType = AbstractGameAction.ActionType.BLOCK;
+        this.energyOnUse = energyOnUse;
+    }
+
+
+    @Override
+    public void update() {
+        int effect = EnergyPanel.totalCount;
+        if (energyOnUse != -1) {
+            effect = this.energyOnUse;
+        }
+
+        if (this.p.hasRelic("Chemical X")) {
+            effect += 2;
+            this.p.getRelic("Chemical X").flash();
+        }
+
+        if (effect > 0) {
+            amount = effect * amount;
+            addToBot(new DamageAction(target, new DamageInfo(p, this.amount), AttackEffect.FIRE));
+            IHelper.getBurn(effect);
+            if (!freeToPlayOnce) {
+                p.energy.use(EnergyPanel.totalCount);
+            }
+        }
+        this.isDone = true;
+    }
+}
---
+++ src/main/java/jaina/cards/ForbiddenFlame.java
@@ -0,0 +1,41 @@
+package jaina.cards;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.actions.ForbiddenFlameAction;
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+
+
+public class ForbiddenFlame extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("ForbiddenFlame");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = -1;
+
+    public ForbiddenFlame() {
+        super(ID, false, CARD_STRINGS, COST, CardType.ATTACK, JainaEnums.JAINA_COLOR,
+                CardRarity.RARE, CardTarget.ENEMY, JainaEnums.CardTags.FIRE);
+        setDamage(10);
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(3);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ForbiddenFlameAction(p, m, damage, freeToPlayOnce, energyOnUse));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new ForbiddenFlame();
+    }
+
+}
