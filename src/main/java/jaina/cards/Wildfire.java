4d796d663604ddfc701814a59c5dc41d2996522c
Arino <dong.yuanhao@outlook.com>
2023-02-05 04:48:54 +0800

    添加了野火

---
+++ src/main/java/jaina/cards/Wildfire.java
@@ -0,0 +1,40 @@
+package jaina.cards;
+
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.powers.WildfirePower;
+
+
+public class Wildfire extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("Wildfire");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = 2;
+
+    public Wildfire() {
+        super(ID, false, CARD_STRINGS, COST, CardType.POWER, JainaEnums.JAINA_COLOR,
+                CardRarity.RARE, CardTarget.NONE, JainaEnums.CardTags.FIRE);
+    }
+
+    @Override
+    public void upp() {
+        upgradeDescription(CARD_STRINGS);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        gainPower(new WildfirePower(p, upgraded));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Wildfire();
+    }
+
+}
---
+++ src/main/java/jaina/powers/WildfirePower.java
@@ -0,0 +1,46 @@
+package jaina.powers;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import jaina.modCore.IHelper;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import jaina.modCore.JainaEnums;
+
+public class WildfirePower extends AbstractJainaPower {
+    public static final String POWER_ID = IHelper.makeID("WildfirePower");
+    public static final String POWER_ID_P = IHelper.makeID("WildfirePowerP");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    private static final String NAME = powerStrings.NAME;
+    private static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private final boolean upgraded;
+
+    public WildfirePower(AbstractCreature owner, boolean upgraded) {
+        super(upgraded ? POWER_ID_P : POWER_ID, true, NAME, PowerType.BUFF);
+        this.owner = owner;
+        this.amount = -1;
+        this.upgraded = upgraded;
+        updateDescription();
+    }
+
+    @Override
+    public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        if (power instanceof BurningPower && !target.isPlayer) {
+            if (upgraded) {
+                power.amount *= 2;
+            } else {
+                power.amount += 3;
+            }
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+
+}
