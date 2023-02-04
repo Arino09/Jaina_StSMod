19711d6d386ce3124bfb4cc7dba79e1647322db1
Arino <dong.yuanhao@outlook.com>
2023-02-05 04:47:23 +0800

    添加了冰刺

---
+++ src/main/java/jaina/cards/Icicle.java
@@ -0,0 +1,45 @@
+package jaina.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+import jaina.powers.FrozenPower;
+
+
+public class Icicle extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("Icicle");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = 0;
+
+    public Icicle() {
+        super(ID, false, CARD_STRINGS, COST, CardType.ATTACK, JainaEnums.JAINA_COLOR,
+                CardRarity.COMMON, CardTarget.ENEMY, JainaEnums.CardTags.FROST);
+        setDamage(4);
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealDamage(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
+        if (m.hasPower(FrozenPower.POWER_ID)) {
+            drawCards(1);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Icicle();
+    }
+
+}
