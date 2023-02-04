9387ce8f2f940d99ca79c3a12d176f01fc5db5cd
Arino <dong.yuanhao@outlook.com>
2023-02-05 04:48:00 +0800

    添加了霜冻射线

---
+++ src/main/java/jaina/cards/RayOfFrost.java
@@ -0,0 +1,62 @@
+package jaina.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.combat.LightRayFlyOutEffect;
+import jaina.modCore.IHelper;
+import jaina.modCore.JainaEnums;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.powers.FrozenPower;
+
+
+public class RayOfFrost extends AbstractJainaCard {
+
+    public static final String ID = IHelper.makeID("RayOfFrost");
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    private static final int COST = 0;
+
+    public RayOfFrost() {
+        super(ID, false, CARD_STRINGS, COST, CardType.ATTACK, JainaEnums.JAINA_COLOR,
+                CardRarity.COMMON, CardTarget.ENEMY, JainaEnums.CardTags.FROST);
+        setDamage(4);
+        setMagicNumber(2);
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        for (AbstractMonster m : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+            if (!m.isDeadOrEscaped() && m.hasPower(FrozenPower.POWER_ID)) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dealDamage(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+        if (m.hasPower(FrozenPower.POWER_ID)) {
+            givePower(new VulnerablePower(m, 2, false), 2);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new RayOfFrost();
+    }
+
+}
