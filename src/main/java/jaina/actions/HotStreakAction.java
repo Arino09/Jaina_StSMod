5e18928c494445b832123bd043f58a44ca6d77f9
Arino <dong.yuanhao@outlook.com>
2023-02-07 16:52:56 +0800

    【炽热连击】重做：添加了一种消耗【灼烧】牌的手段

---
+++ src/main/java/jaina/actions/HotStreakAction.java
@@ -0,0 +1,46 @@
+package jaina.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.Burn;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.powers.BurningPower;
+
+import java.util.ArrayList;
+
+public class HotStreakAction extends AbstractGameAction {
+
+    public HotStreakAction(int burningAmt) {
+        this.startDuration = this.duration = Settings.ACTION_DUR_FAST;
+        this.actionType = ActionType.WAIT;
+        this.amount = burningAmt;
+    }
+
+    @Override
+    public void update() {
+        AbstractPlayer p = AbstractDungeon.player;
+        ArrayList<AbstractCard> cardsToExhaust = new ArrayList<>();
+        // 构造灼烧牌列表
+        for (AbstractCard c : p.hand.group) {
+            if (c instanceof Burn) {
+                cardsToExhaust.add(c);
+            }
+        }
+        // 每灼烧一张给予所有敌人amount层灼烧
+        for (AbstractCard c : cardsToExhaust) {
+            for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+                addToTop(new ApplyPowerAction(m, p, new BurningPower(m, amount)));
+            }
+        }
+        // 先消耗所有灼烧牌
+        for (AbstractCard c : cardsToExhaust) {
+            addToTop(new ExhaustSpecificCardAction(c, p.hand));
+        }
+    }
+
+}
--- src/main/java/jaina/cards/HotStreak.java
+++ src/main/java/jaina/cards/HotStreak.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import jaina.actions.HotStreakAction;
 import jaina.modCore.IHelper;
 import jaina.modCore.JainaEnums;
 import jaina.powers.HotStreakPower;
@@ -15,23 +16,22 @@ public class HotStreak extends AbstractJainaCard {
     public static final String ID = IHelper.makeID("HotStreak");
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    private static final int COST = 0;
+    private static final int COST = 1;
 
     public HotStreak() {
         super(ID, false, CARD_STRINGS, COST, CardType.SKILL, JainaEnums.JAINA_COLOR,
                 CardRarity.COMMON, CardTarget.NONE, JainaEnums.CardTags.FIRE);
-        this.exhaust = true;
+        setMagicNumber(1);
     }
 
     @Override
     public void upp() {
-        this.exhaust = false;
-        upgradeDescription(CARD_STRINGS);
+        upgradeMagicNumber(1);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        gainPower(new HotStreakPower(p));
+        addToBot(new HotStreakAction(magicNumber));
     }
 
     @Override
