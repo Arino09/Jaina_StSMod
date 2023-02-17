f2d8f708d3632df4eb6eb613b568147c77a74317
Arino <dong.yuanhao@outlook.com>
2023-02-17 19:35:20 +0800

    寒冰屏障现在在触发时才会闪烁，并闪烁格挡动画

--- src/main/java/jaina/powers/unique/IceBlockPower.java
+++ src/main/java/jaina/powers/unique/IceBlockPower.java
@@ -1,11 +1,13 @@
 package jaina.powers.unique;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 import jaina.modCore.IHelper;
 import jaina.powers.AbstractJainaPower;
 
@@ -30,9 +32,10 @@ public class IceBlockPower extends AbstractJainaPower {
     @Override
     public int onAttacked(DamageInfo info, int damageAmount) {
         if (info.type == DamageInfo.DamageType.NORMAL && info.owner != null && info.owner != this.owner) {
-            flash();
             if (owner.currentHealth + owner.currentBlock <= damageAmount) {
-                AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+                flash();
+                addToBot(new VFXAction(new FlashAtkImgEffect(owner.hb_x, owner.hb_y, AttackEffect.SHIELD)));
+                addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
                 return 0;
             }
         }
